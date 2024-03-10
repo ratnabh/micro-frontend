badbe3a41449d31439abb1171cd14009257f7699
ratnabh-classic <ratnabh.rai@classicinformatics.com>
2024-03-10 12:15:03 +0530

    fix paths

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 8081,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "products",
+      filename: "remoteEntryProduct.js",
+      exposes: {
+        "./ProductsIndex": "./src/index.js",
+      },
+      // shared: { react: { singleton: true }, "react-dom": { singleton: true } },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
